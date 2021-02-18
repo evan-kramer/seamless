4af6d251edb53b999858fc8edca8c16c60c55ffc
Evan Kramer <kramer.evan@gmail.com>
2021-02-18 09:23:41 -0500

    Cleaning up examples and key testing

--- seamless.py
+++ seamless.py
@@ -5,7 +5,7 @@ Created on Thu Jan 28 22:12:45 2021
 Pull Data from Seamless
 """
 # Import modules
-import os, requests, hashlib, hmac, base64, time
+import os, requests, hashlib, hmac, time
 # from urllib.request import urlopen, Request
 import pandas as pd
 
@@ -17,22 +17,6 @@ def get_signature(domain, url, method, uri):
                          bytes(message, 'utf-8'),
                          digestmod = hashlib.sha256).hexdigest()
     return signature
-
-# Example
-base64.b64encode(hmac.new(bytes('uq7UKtK4NEBVqNPbHBTImuxxShp8ug', 'utf-8'),
-                          bytes('POST+/form/CO15021000011408891/elements+1425589564+abc123', 'utf-8'),
-                          digestmod = hashlib.sha256).digest())
-hmac.new(bytes('uq7UKtK4NEBVqNPbHBTImuxxShp8ug', 'utf-8'),
-         bytes('POST+/form/CO15021000011408891/elements+1425589564+abc123', 'utf-8'),
-         digestmod = hashlib.sha256).hexdigest()
-
-'0cdba187c5a2ce12f732ccbdb12a0c4d82cf8930f1d6aedef58cb38dcf9c6919'
-
-
-
-
-
-
 
 # Get all forms
 domain = 'dcgov'
@@ -61,13 +45,6 @@ except:
 # print(get_signature(domain, url, 'GET', uri))
 # https://www.freeformatter.com/hmac-generator.html#ad-output
 
-
-'''
-curl -X GET -H "Content-Type: application/json" 
--H "Authorization: api_key=pXW4hc3JH7w3lc1Z" 
--H "AuthDate: 1611928508" -H "Cache-Control: no-cache" 
--d '' https://sandbox.seamlessdocs.com/api/account/forms
-'''
 
 # Budget package requests
 # Data sharing agreement requests
